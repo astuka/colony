85da112abb5ae42fad85d5ee7fde643878bbde82
astuka <jrobins855@gmail.com>
2022-01-31 12:24:20 -0700

    Core loop

--- core.py
+++ core.py
@@ -1,9 +1,39 @@
 import generator as g
 import events as e 
+import random as r
 
-roster = {} #list of all living characters
+roster = [] #list of all living characters
 
 #spawn in initial colony
+for x in range(5):
+    x = g.generation()
+    roster.append(x)
 
-person1 = g.generation()
-person2 = g.generation()
+
+
+#gameplay loop
+while True:
+    print("What would you like to do? \n 1. Roster Lookup \n 2. Proceed with a new year")
+    i = input()
+    if i == "1":
+        for r in roster:
+            print(r.firstname+" "+r.lastname+", a level "+str(r.level)+" "+r.clas)
+    elif i == "2":
+        #new year default events
+        #Age increase
+        for r in roster:
+            r.age += 1
+        #Natural death chance
+        for i in roster:
+            if i.age < 65:
+                death_chance = 0.05
+                roll = r.random
+                if roll <= death_chance:
+                    print(i.firstname+" has died of natural causes.")
+                    roster.remove(i)
+            else:
+                death_chance = 0.2
+                roll = r.random
+                if roll <= death_chance:
+                    print(i.firstname+" has died of natural causes.")
+                    roster.remove(i)
--- generator.py
+++ generator.py
@@ -6,29 +6,42 @@ gender = ["Male", "Female"]
 m_firstnames = ["George", "John", "Davis"]
 f_firstnames = ["Marie", "Susie", "Sarah"]
 lastnames = ["Whitebottom", "Ferriss", "Johnson"]
+classes = ["Fighter", "Mage", "Ranger", "Merchant", "Politician" ]
+alignments = ["Good", "Neutral", "Evil"]
 
 class Character:
-  def __init__(self, gender, firstname, lastname, age, level):
+  def __init__(self, gender, firstname, lastname, age, level, clas, alignment):
     self.gender = gender
     self.firstname = firstname
     self.lastname = lastname
     self.age = age
     self.level = level
+    self.clas = clas
+    self.alignment = alignment
 
 #generation
 
 def generation():
+    #choose gender
     chose_gender = gender[r.randrange(0,2)]
 
+    #choose first name, based on gender
     if chose_gender == "Male":
         chose_first = m_firstnames[r.randrange(0,3)]
     else:
         chose_first = f_firstnames[r.randrange(0,3)]
 
+    #choose last name
     chose_last = lastnames[r.randrange(0,3)]
 
     #age is random for now, but later will change for birth events
-    chose_age = str(r.randrange(0,41))
+    chose_age = r.randrange(0,41)
+
+    #choose class
+    choose_class = classes[r.randrange(0,5)]
+
+    #choose alignment
+    choose_alignment = alignments[r.randrange(0,3)]
     
-    generated = Character(chose_gender,chose_first,chose_last,chose_age, 1)
+    generated = Character(chose_gender,chose_first,chose_last,chose_age, 1, choose_class, choose_alignment)
     return generated
